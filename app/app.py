fa676f1720bf315034ca3bc7fa6975deb8319dcd
Eshal Fathima <eshalf483@gmail.com>
2025-09-14 09:10:13 +0530

    Initial Flask app with IBM Watson placeholders

--- app/app.py
+++ app/app.py
@@ -8,3 +8,11 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+from ibm_watson import SpeechToTextV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+
+authenticator = IAMAuthenticator(os.getenv("IBM_API_KEY"))
+speech_to_text = SpeechToTextV1(authenticator=authenticator)
+speech_to_text.set_service_url(os.getenv("IBM_URL"))
